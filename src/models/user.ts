e568693db67e0d79039f76a3b3af41fb32eeca0a
Ryokuman <ryokuman@naver.com>
2023-05-31 14:10:49 +0900

    chore ( model ) :  user
    change to make it run in typescript

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,25 +1,36 @@
-import { DataTypes } from "sequelize";
-import sequelize from "@utils/connect.js";
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "./index";
 
-const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    primaryKey: true,
-  },
-  userID: {
-    type: DataTypes.STRING(30),
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING(50),
-    unique: true,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING(20),
-    allowNull: false,
-  },
-});
+// These are all the attributes in the User model
+interface UsersAttributes {
+  email: string;
+  password: string | null;
+}
 
-export default User;
+export class Users extends Model<UsersAttributes> {
+  public email!: string;
+  public password!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Users.init(
+  {
+    email: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(45),
+      allowNull: true,
+    },
+  },
+  {
+    modelName: "Users",
+    tableName: "Users",
+    sequelize,
+    freezeTableName: true,
+    timestamps: true,
+    updatedAt: "updateTimestamp",
+  }
+);
