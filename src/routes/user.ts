cf52688317a639ec4fa7089e9a5f065b747e70ad
Ryokuman <ryokuman@naver.com>
2023-07-08 15:36:41 +0300

    feature ( route ) : add jwt to user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,17 +3,31 @@ import { User } from "@models/user";
 import { v4 } from "uuid";
 import { makePw, checkPw } from "@utils/pwUtil";
 import { UniqueConstraintError } from "sequelize";
+import { sign } from "jsonwebtoken";
+import { jwt } from "src/config/config";
 
 const user = Router();
 
 user.post("/signin", async (req, res) => {
   try {
     const result = await User.findOne({ where: { email: req.body.email } });
-    if (result && (await checkPw(req.body.password, result.dataValues.password, result.dataValues.salt)))
-      res.status(200).send("success");
-    else res.status(401).send("wrong value");
+
+    if (result && (await checkPw(req.body.password, result.dataValues.password, result.dataValues.salt))) {
+      const acToken = sign({ id: result.dataValues.id }, jwt.secret, {
+        algorithm: jwt.algorithm,
+        expiresIn: 60 * 60 + 24,
+      });
+
+      const rfToken = sign({ id: result.dataValues.id }, jwt.secret, {
+        algorithm: jwt.algorithm,
+        expiresIn: 60 * 60 + 24 * 14,
+      });
+
+      res.status(200).json({ acToken, rfToken });
+    } else res.status(401).send("wrong value");
   } catch (error) {
-    res.send(error);
+    console.log(error);
+    res.status(401).send(error);
   }
 });
 
