308b5599e400091862633cd5744e880029f85b85
Ryokuman <ryokuman@naver.com>
2023-07-08 15:51:11 +0300

    chore ( route ) : add  get to user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,28 @@
 import { Router } from "express";
-import { User } from "@models/user";
-import { v4 } from "uuid";
-import { makePw, checkPw } from "@utils/pwUtil";
 import { UniqueConstraintError } from "sequelize";
-import { sign } from "jsonwebtoken";
+import { v4 } from "uuid";
+import { sign, verify } from "jsonwebtoken";
+
+import { User } from "@models/user";
+import { makePw, checkPw } from "@utils/pwUtil";
 import { jwt } from "src/config/config";
 
+type decodedType = { id: string; iat: number; exp: number };
+
 const user = Router();
+
+user.get("", async (req, res) => {
+  try {
+    if (req.headers.authorization)
+      verify(req.headers.authorization, jwt.secret, async (err, decoded) => {
+        const data = <decodedType>decoded;
+        const result = await User.findOne({ where: { id: data.id } });
+        if (result) res.send(result.dataValues.email);
+      });
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 user.post("/signin", async (req, res) => {
   try {
